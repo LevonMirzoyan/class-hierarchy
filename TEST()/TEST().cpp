8d43ea41fb5476b6c9e68e9a8767bb575e8fc531
LevonMirzoyan <83034426+LevonMirzoyan@users.noreply.github.com>
2021-05-27 10:19:13 +0400

    Update TEST().cpp

--- TEST()/TEST().cpp
+++ TEST()/TEST().cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 //------------- Абстрактные продукты ---------------
@@ -11,9 +11,9 @@ struct Dinner { //абстрактный продукт
 };
 
 
-struct Diet { // абстрактный продукт - вообще это должна была быть фабрика
-    virtual void eat() = 0;
-};
+//struct Diet { // абстрактный продукт - вообще это должна была быть фабрика
+ //   virtual void eat() = 0;
+//};
 
 //------------- Конкретные продукты-----------------
 
@@ -45,105 +45,81 @@ struct Cutlet : Dinner {
     }
 };
 
-//  наследники
-struct MeatDiet : Diet { //вот это тоже должна была быть фабрика
-    virtual void eat()
-    {
-        cout << "MeatDiet cook" << endl;
-    }
-};
 
-
-struct VegetarianDiet : Diet { // и это тоже подразумевалось фабрикой
-    virtual void eat()
-    {
-        cout << "VegetarianDiet cook" << endl;
-    }
-};
 
 //------------------ Абстрактная фабрика ----------------------
 
-struct AbstractFactory {
+struct Diet {
     virtual Soup* createSoup() = 0; //создаем абстрактный продукт
     virtual Dinner* createDinner() = 0; //создаем абстрактный продукт
     //при добавлении нового продукта придется добавить новый метод для его создания
-    virtual Diet* createDiet() = 0; // т.к. диета подразумевалась фабрикой, то тут ее не надо было создавать
+  //  virtual Diet* createDiet() = 0; // т.к. диета подразумевалась фабрикой, то тут ее не надо было создавать
 };
 
 // ------------------ Конкретные фабрики ----------------------
 
-struct SillyFactory : AbstractFactory { //вот это должна была быть фабрика МяснаяДиета
+struct MeatDiet : Diet { 
     virtual Soup* createSoup() {
         return new Borsch(); //создаем экземпляр конкретного продукта 
     }
-
+    
     virtual Dinner* createDinner() {
-        return new Fried_potato(); //создаем экземпляр конкретного продукта  - т.к. диета мясная, то создавать надо было котлетку
+        return new Cutlet(); //создаем экземпляр конкретного продукта 
     }
 
-    //добавить метод создания его конкретного наследника
-    virtual Diet* createDiet() {
-        return new MeatDiet(); //создает экземпляр конкретного продукта  - т.к. диета - это фабрика, а тут создается продукт, то ее не надо было
-    }
 };
 
-struct SmartFactory : AbstractFactory { // вот это должна быть фабрика ВегетарианскаяДиета
+struct VegetarianDiet : Diet { // вот это должна быть фабрика ВегетарианскаяДиета
     virtual  Soup* createSoup() {
         return new Pumpkin(); //создаем экземпляр конкретного продукта 
     }
-
     virtual Dinner* createDinner() {
-        return new Cutlet(); //создаем экземпляр конкретного продукта  - т.к. диета вегетарианская, то тут надо было создавать жареную картошку
+        return new Fried_potato(); //создаем экземпляр конкретного продукта  
     }
 
-    //добавить метод создания его конкретного наследника
-    virtual Diet* createDiet() { //т.к. диета - это фабрика, а создавать тут надо продукт, то этого метода тут быть не должно
-        return new VegetarianDiet();
-    }
+   
+
 };
 
 
 int main() {
 
-    AbstractFactory* factory = new SillyFactory();
+    Diet* factory = new MeatDiet(); 
 
     Soup* m1 = factory->createSoup();
-    Soup* m2 = factory->createSoup();
-    Dinner* Dinner = factory->createDinner();
-    //добавим новый продукт 
-    Diet* d = factory->createDiet();
-
-    m1->eat();
-    m2->eat();
-    Dinner->eat();
-    //новый продукт
-    d->eat();
-
-    delete factory;
-    delete m1;
-    delete m2;
-    delete Dinner;
-    delete d; //освобождаем память 
-
-    factory = new SmartFactory(); 
-    m1 = factory->createSoup();
-    m2 = factory->createSoup();
-    Dinner = factory->createDinner();
     
-    d = factory->createDiet();
+    Dinner* Dinner = factory->createDinner();
+    
+    cout << '\n' << "MeatDiet :"  << endl;
 
-    cout << "-----------------------" << endl;
     m1->eat();
-    m2->eat();
+    
     Dinner->eat();
-    //новый продукт
-    d->eat();
+    
 
     delete factory;
     delete m1;
-    delete m2;
     delete Dinner;
-    delete d;//освободим память
+   
+
+    factory = new VegetarianDiet();
+    m1 = factory->createSoup();
+    
+    Dinner = factory->createDinner();
+
+ 
+
+    cout << '\n' << "VegetarianDiet: "  << endl;
+    m1->eat();
+    
+    Dinner->eat();
+   
+
+    delete factory;
+    delete m1;
+    
+    delete Dinner;
+ 
 
     return 0;
 }
